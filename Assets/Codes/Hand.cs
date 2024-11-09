19a7e93e9eff02b9fa2977f57cd148f48b4f1a12
undong <ehddjs85246@gmail.com>
2024-11-09 13:13:29 +0900

    이미지 수정
    이미지 수정

--- Assets/Codes/Hand.cs
+++ Assets/Codes/Hand.cs
@@ -10,10 +10,10 @@ public class Hand : MonoBehaviour {
 
     SpriteRenderer player;
 
-    Vector3 rightPos = new Vector3(0.35f, -0.15f, 0);
-    Vector3 rightPosReverse = new Vector3(-0.15f, -0.15f, 0);
-    Quaternion leftRot = Quaternion.Euler(0, 0, -35);
-    Quaternion leftRotReverse = Quaternion.Euler(0, 0, -135);
+    Vector3 rightPos = new Vector3(0.45f, -0.25f, 0);
+    Vector3 rightPosReverse = new Vector3(-0.25f, -0.25f, 0);
+    Quaternion leftRot = Quaternion.Euler(0, 0, -90);
+    Quaternion leftRotReverse = Quaternion.Euler(0, 0, -90);
 
     void Awake() {
         player = GetComponentsInParent<SpriteRenderer>()[1];
@@ -25,12 +25,12 @@ public class Hand : MonoBehaviour {
         if (isLeft) {   //근접 무기
             transform.localRotation = isReverse ? leftRotReverse : leftRot;
             spriter.flipY = isReverse;
-            spriter.sortingOrder = isReverse ? 4 : 6;
+            spriter.sortingOrder = isReverse ? 6 : 4;
         }
         else {  //원거리 무기
             transform.localPosition = isReverse ? rightPosReverse : rightPos;
             spriter.flipX = isReverse;
-            spriter.sortingOrder = isReverse ? 6 : 4;
+            spriter.sortingOrder = isReverse ? 4 : 6;
         }
     }
 }
